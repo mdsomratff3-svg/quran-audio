28271d9da1c901852e89da92e2519bc05e7ee79e
mdsomratff3-svg <mdsomratff3@gmail.com>
2026-09-11 13:53:01 +0600

    Create generate_audio_api.php with audio URLs
    Added a JSON object containing audio file URLs.

---
+++ generate_audio_api.php
@@ -0,0 +1,7 @@
+{
+  "1": "https://github.com/mdsomratff3-svg/quran-audio/releases/download/v1.0/001.mp3",
+  "2": "https://github.com/mdsomratff3-svg/quran-audio/releases/download/v1.0/002.mp3",
+  "3": "https://github.com/mdsomratff3-svg/quran-audio/releases/download/v1.0/003.mp3",
+  ...
+  "114": "https://github.com/mdsomratff3-svg/quran-audio/releases/download/v1.0/114.mp3"
+}
